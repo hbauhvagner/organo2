6a6663e8e2ea5cb2380a4ada85de92c0423f5070
hbauhvagner <hbauhvagner@gmail.com>
2023-08-24 00:39:46 -0300

    Criando ids

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 import Rodape from './componentes/Rodape';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: 'Programação',
       cor: '#57C278'
     },
     {
+      id: uuidv4(),
       nome: 'Front-End',
       cor: '#82CFFA'
     },
     {
+      id: uuidv4(),
       nome: 'Data Science',
       cor: '#A6D157'
     },
     {
+      id: uuidv4(),
       nome: 'Devops',
       cor: '#E06B69'
     },
     {
+      id: uuidv4(),
       nome: 'UX e Design',
       cor: '#DB6EBF'
     },
     {
+      id: uuidv4(),
       nome: 'Mobile',
       cor: '#FFBA05'
     },
     {
+      id: uuidv4(),
       nome: 'Inovação e Gestão',
       cor: '#FF8A29'
     }
@@ -43,9 +51,9 @@ function App() {
     console.log('deletando colaborador')
   }
 
-  function mudarCor(cor, nome) {
+  function mudarCor(cor, id) {
     setTimes(times.map(time => {
-      if (time.nome === nome) {
+      if (time.id === id) {
         time.cor = cor;
       }
 
